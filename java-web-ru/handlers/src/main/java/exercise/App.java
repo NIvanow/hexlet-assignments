e3e15c021fed51fd3a2de3d12ed98718f0be86e4
@hexlet/cli <support@hexlet.io>
2024-12-08 01:04:59 +0500

    submit java-web-ru/handlers

--- java-web-ru/handlers/src/main/java/exercise/App.java
+++ java-web-ru/handlers/src/main/java/exercise/App.java
@@ -16,7 +16,7 @@ public final class App {
         });
 
         app.get("/phones", ctx -> ctx.json(phones));
-        app.get("/domains", ctx -> ctx.json( domains));
+        app.get("/domains", ctx -> ctx.json(domains));
         app.start(7070);
 
         return app;
